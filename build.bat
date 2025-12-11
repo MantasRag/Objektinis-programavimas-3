181600c138ffc3d7c542fcd873054063b84f8dfd
Mantas Ragaišis <ragaisis.m@gmail.com>
2025-12-11 17:59:37 +0200

    Add files via upload
    Įkeltas pakeistas CMakeLists.txt ir build.bat.

--- build.bat
+++ build.bat
@@ -5,6 +5,10 @@ if not exist build mkdir build
 
 cd build
 
+REM Išvalyti senus build failus
+del /Q *.exe >nul 2>nul
+del /Q *.obj >nul 2>nul
+
 REM Generuoti ir kompiliuoti projektą
 cmake .. -G "MinGW Makefiles" || (echo CMake konfiguracija nepavyko & cd .. & pause & exit /b 1)
 cmake --build . --config Release || (echo Kompiliavimas nepavyko & cd .. & pause & exit /b 1)
